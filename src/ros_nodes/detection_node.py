6def563766b14153a3704d2f8b245154fc754603
Chris Smalley <chris.smalley.32@gmail.com>
2017-05-13 20:18:45 -0400

    Added start of some more nodes. Long way to go

---
+++ src/ros_nodes/detection_node.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+from collections import namedtuple
+import sys
+# import time
+try:
+    import ConfigParser
+except ImportError:
+    import configparser as ConfigParser
+import numpy as np
+# import random
+import rospy
+# from sensor_msgs.msg import LaserScan
+# from geometry_msgs.msg import Twist
+# import tf
+import models
+
+# from robot-motion-planning import utils
+
+
+class RosDetectionNode(object):  # {{{1
+
+    """
+    Create a ROS node that implements a object detection algorithms
+    inputs: All inputs should be from from sensors? (camera, lidar..)
+    outputs: Outputs should be obstacles (type TBD)
+    """
+    Topic = namedtuple("Topic",
+                       ["name",
+                        "type",
+                        "callback"])
+#    ParameterVector = namedtuple("DiffDriveRosPid",
+#                                 ["k_p",
+#                                  "k_i",
+#                                  "k_d"])
+
+    def __init__(self, subscribe_topics, publish_topics, model, paramdescr=None):  # {{{2
+        self.set_param(descr["policyparam"])
+        # SET UP RATE FOR LOOP
+        rate = rospy.Rate(10)  # TODO add to config
+        # SET UP PUBLISHING
+        self.pub = rospy.Publisher(publish_topic.name,
+                                   publish_topic.type)
+        # SET UP SUBSCRIBING
+        # rospy.Subscriber("base_scan", LaserScan, self.laserScanCallback)
+        for topic in subscribe_topics:
+            rospy.Subscriber(topic.name,
+                             topic.type,
+                             eval("self." + topic.callback))
+        # SET UP ALGORITHM FOR NODE
+        # Set model used in policy
+        # self.q = eval("models." + model + "((0.0, 0.0, 0.0), (0.25,))")
+        while not rospy.is_shutdown():
+            data = self.policy.step()
+            # TODO May need to repackage data
+            self.pub.publish(data)
+            rate.sleep()
+
+    # Callback functions {{{2
+    def cameraCallback(self, data):  # {{{3
+        pass
+
+    def laserScanCallback(self, data):  # {{{3
+        angleMin = data.angle_min
+        inc = data.angle_increment
+        phi = angleMin
+        self.laserscan = []
+        for i, d in enumerate(data.ranges):
+            if d == data.range_max:
+                continue
+            phi = angleMin + inc * i
+            rho = d
+            x = rho * np.cos(phi)
+            y = rho * np.sin(phi)
+            scan = models.Point2D()
+            scan.set_q((x, y))
+            self.laserscan.append(scan)
+        print("laserscan: ", self.laserscan[0])
+
+    # Publish functions {{{2
+    def publishObjects(self, pub, data):  # {{{3
+        print("name: ", pub.name)
+        print("data_class: ", pub.data_class)
+        pub.publish(data)
+
+# Main function.
+if __name__ == '__main__':  # {{{1
+    # READ ARGS FOR HOW TO CONFIGURE POLICY
+    configfilename = sys.argv[1]
+    print("\n\nconfigfilename: ", configfilename)
+    config = ConfigParser.ConfigParser()
+    config.read(configfilename)
+    descr = eval(config.get("Policy", "descr"))
+    model = descr["model"]
+    policyname = descr["policyname"]
+    publish_topic = descr["publish_topic"]
+    print("publish_topic: ", publish_topic)
+    subscribe_topics = descr["subscribe_topics"]
+    print("subscribe_topics: ", subscribe_topics)
+    # Initialize the node and name it.
+    rospy.init_node(policyname)
+    # Go to class functions that do all the heavy lifting. Do error checking.
+    try:
+        ne = RosDetectionNode(subscribe_topics, publish_topic, model, descr)
+    except rospy.ROSInterruptException:
+        pass
--- src/ros_nodes/mapping_node.py
+++ src/ros_nodes/mapping_node.py
@@ -1,69 +1,63 @@
 #!/usr/bin/python
 from collections import namedtuple
 import sys
-import time
+# import time
 try:
     import ConfigParser
 except ImportError:
     import configparser as ConfigParser
 import numpy as np
-import random
+# import random
 import rospy
-from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
-import tf
+# from sensor_msgs.msg import LaserScan
+# from geometry_msgs.msg import Twist
+# import tf
 import models
 
-import utils
+# from robot-motion-planning import utils
 
 
-class RosPolicyNode(object):  # {{{1
+class RosMappingNode(object):  # {{{1
 
     """
-    Create a ROS node that implements a policy
+    Create a ROS node that implements a object detection algorithms
+    inputs: All inputs should be from from sensors? (camera, lidar..)
+    outputs: Outputs should be obstacles (type TBD)
     """
-    ParameterVector = namedtuple("DiffDriveRosPid",
-                                 ["k_p",
-                                  "k_i",
-                                  "k_d"])
+    Topic = namedtuple("Topic",
+                       ["name",
+                        "type",
+                        "callback"])
+#    ParameterVector = namedtuple("DiffDriveRosPid",
+#                                 ["k_p",
+#                                  "k_i",
+#                                  "k_d"])
 
     def __init__(self, subscribe_topics, publish_topics, model, paramdescr=None):  # {{{2
         self.set_param(descr["policyparam"])
-        # Set up rate for loop
+        # SET UP RATE FOR LOOP
         rate = rospy.Rate(10)  # TODO add to config
-        # Set up publishing
-        self.publishers = []
-        for topic in publish_topics:
-            # topic (name, message type)
-            pub = rospy.Publisher(topic[0], topic[1])
-            self.publishers.append(pub)
-        # Set up subscribing
+        # SET UP PUBLISHING
+        self.pub = rospy.Publisher(publish_topic.name,
+                                   publish_topic.type)
+        # SET UP SUBSCRIBING
+        # rospy.Subscriber("base_scan", LaserScan, self.laserScanCallback)
         for topic in subscribe_topics:
-            if topic == "base_scan":
-                rospy.Subscriber("base_scan", LaserScan, self.laserScanCallback)
-            elif topic == "odom":
-                rospy.Subscriber("odom", Odometry, self.odomCallback)
-        # self.policy = policy
+            rospy.Subscriber(topic.name,
+                             topic.type,
+                             eval("self." + topic.callback))
+        # SET UP ALGORITHM FOR NODE
         # Set model used in policy
-        # self.q_start = (0.0, 0.0, 0.0)
-        # while self.q is None:
-        #     print "waiting to subscribe to odom"
-        #     time.sleep(0.2)
-        self.q = eval("models." + model + "((0.0, 0.0, 0.0), (0.25,))")
-        # self.q = model
+        # self.q = eval("models." + model + "((0.0, 0.0, 0.0), (0.25,))")
         while not rospy.is_shutdown():
             data = self.policy.step()
             # TODO May need to repackage data
             self.pub.publish(data)
             rate.sleep()
 
-    def set_param(self, paramdescr):  # {{{2
-        try:
-            self.param = self.ParameterVector(**paramdescr)
-        except TypeError:
-            self.param = None
-        except AttributeError:
-            self.param = None
+    # Callback functions {{{2
+    def cameraCallback(self, data):  # {{{3
+        pass
 
     # Callback functions {{{2
     def odomCallback(self, data):  # {{{3
@@ -80,7 +74,7 @@ class RosPolicyNode(object):  # {{{1
                   euler[2])
         print "q: ", self.q
 
-    def laserScanCallback(self, data):  # {{{3
+    def ObjectCallback(self, data):  # {{{3
         angleMin = data.angle_min
         inc = data.angle_increment
         phi = angleMin
@@ -95,31 +89,46 @@ class RosPolicyNode(object):  # {{{1
             scan = models.Point2D()
             scan.set_q((x, y))
             self.laserscan.append(scan)
-        print "laserscan: ", self.laserscan[0]
+        print("laserscan: ", self.laserscan[0])
 
     # Publish functions {{{2
-    def publish(self, pub, data):  # {{{3
-        print "name: ", pub.name
-        print "data_class: ", pub.data_class
+    def publishMap(self, mapPub, data):  # {{{3
+        print("name: ", pub.name)
+        print("data_class: ", pub.data_class)
+        pub.publish(data)
+
+    def publishPose(self, posePub, data):  # {{{3
+        pass
 
 # Main function.
 if __name__ == '__main__':  # {{{1
+    """
+    Mapping node:
+        inputs:
+            current map
+            objects
+            estimated orientation
+        output:
+            updated map
+            updated orientation
+    """
     # READ ARGS FOR HOW TO CONFIGURE POLICY
     configfilename = sys.argv[1]
-    print "\n\nconfigfilename: ", configfilename
+    print("\n\nconfigfilename: ", configfilename)
     config = ConfigParser.ConfigParser()
     config.read(configfilename)
     descr = eval(config.get("Policy", "descr"))
     model = descr["model"]
     policyname = descr["policyname"]
-    publish_topics = descr["publish_topics"]
-    print "publish_topics: ", publish_topics
+    publish_topic = descr["publish_topic"]
+    print("publish_topic: ", publish_topic)
     subscribe_topics = descr["subscribe_topics"]
-    print "subscribe_topics: ", subscribe_topics
+    print("subscribe_topics: ", subscribe_topics)
     # Initialize the node and name it.
     rospy.init_node(policyname)
     # Go to class functions that do all the heavy lifting. Do error checking.
     try:
-        ne = RosPolicyNode(subscribe_topics, publish_topics, model, descr)
+        ne = RosDetectionNode(subscribe_topics, publish_topic, model, descr)
     except rospy.ROSInterruptException:
         pass
+
